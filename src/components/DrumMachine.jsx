ab2fd778551cd5ab0832433cda3f441df4f86c2f
Vanessa <110750471+nattdev@users.noreply.github.com>
2023-12-29 20:09:33 -0500

    Implement drum-pad keyDown functionality to trigger audio clip

--- src/components/DrumMachine.jsx
+++ src/components/DrumMachine.jsx
@@ -3,16 +3,10 @@ import PadBank from "./PadBank";
 
 function DrumMachine() {
 
-    const handlePlayAudio = (event) => {
-        const audio = event.target.firstElementChild;
-        audio.currentTime = 0;
-        audio.play();
-    };
-
     return (
         <div id="drum-machine">
             <Controls />
-            <PadBank onClick={handlePlayAudio}/>
+            <PadBank />
         </div>
     )
 }
--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,7 +1,37 @@
-function DrumPad({id, text, src, name, onClick}) {
+import { useRef } from "react";
+import { useEffect } from "react";
+
+function DrumPad({ id, text, name }) {
+
+    const audioRef = useRef();
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+    const handleKeyDown = (event) => {
+        const key = event.key.toUpperCase();
+        const audio = audioRef.current;
+
+        if (key == id) {
+            audio.currentTime = 0;
+            audio.play();
+        }
+    }
+
+    const handlePlayAudio = () => {
+        const audio = audioRef.current;
+
+        audio.currentTime = 0;
+        audio.play();
+    };
+
     return (
-        <div id={name} onClick={onClick} className="drum-pad">
-            <audio id={id} className="clip" src={`src/assets/${name}.mp3`}></audio>
+        <div id={name} onClick={handlePlayAudio} className="drum-pad">
+            <audio ref={audioRef} id={id} className="clip" src={`src/assets/${name}.mp3`}></audio>
             {text}
         </div>
     )
